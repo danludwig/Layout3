1e78a1cb79ae127c7fe53e2a6f56c48522d63740
Douglas Corarito <corarito@usf.edu>
2013-04-09 11:14:38 -0400

    Code Review 4/5/2013 Changes - Part 3 ReSharper

--- UCosmic.Infrastructure/SeedData/FileEntitySeeder.cs
+++ UCosmic.Infrastructure/SeedData/FileEntitySeeder.cs
@@ -28,7 +28,7 @@ namespace UCosmic.SeedData
             extension = extension != null ? extension.Substring(1) : null;
             if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             {
-                using (FileStream fileStream = File.OpenRead(basePath + filename))
+                using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}",basePath,filename)))
                 {
                     Seed(new CreateLoadableFile
                     {
@@ -47,7 +47,7 @@ namespace UCosmic.SeedData
             extension = extension != null ? extension.Substring(1) : null;
             if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             {
-                using (FileStream fileStream = File.OpenRead(basePath + filename))
+                using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}", basePath, filename)))
                 {
                     Seed(new CreateLoadableFile
                     {
@@ -66,7 +66,7 @@ namespace UCosmic.SeedData
             extension = extension != null ? extension.Substring(1) : null;
             if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             {
-                using (FileStream fileStream = File.OpenRead(basePath + filename))
+                using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}", basePath, filename)))
                 {
                     Seed(new CreateLoadableFile
                     {
@@ -84,7 +84,7 @@ namespace UCosmic.SeedData
             //extension = Path.GetExtension(filename).Substring(1);
             //if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             //{
-            //    using (FileStream fileStream = File.OpenRead(basePath + filename))
+            //    using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}",basePath,filename)))
             //    {
             //        Seed(new CreateLoadableFile
             //        {
@@ -102,7 +102,7 @@ namespace UCosmic.SeedData
             //extension = Path.GetExtension(filename).Substring(1);
             //if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             //{
-            //    using (FileStream fileStream = File.OpenRead(basePath + filename))
+            //    using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}",basePath,filename)))
             //    {
             //        Seed(new CreateLoadableFile
             //        {
@@ -120,7 +120,7 @@ namespace UCosmic.SeedData
             //extension = Path.GetExtension(filename).Substring(1);
             //if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             //{
-            //    using (FileStream fileStream = File.OpenRead(basePath + filename))
+            //    using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}",basePath,filename)))
             //    {
             //        Seed(new CreateLoadableFile
             //        {
@@ -138,7 +138,7 @@ namespace UCosmic.SeedData
             //extension = Path.GetExtension(filename).Substring(1);
             //if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             //{
-            //    using (FileStream fileStream = File.OpenRead(basePath + filename))
+            //    using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}",basePath,filename)))
             //    {
             //        Seed(new CreateLoadableFile
             //        {
@@ -157,7 +157,7 @@ namespace UCosmic.SeedData
             extension = extension != null ? extension.Substring(1) : null;
             if (_entities.Get<LoadableFile>().Count(x => x.Name == name) == 0)
             {
-                using (FileStream fileStream = File.OpenRead(basePath + filename))
+                using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}", basePath, filename)))
                 {
                     Seed(new CreateLoadableFile
                     {
@@ -176,7 +176,7 @@ namespace UCosmic.SeedData
             extension = extension != null ? extension.Substring(1) : null;
             if (_entities.Get<LoadableFile>().Count(x => x.Name == filename) == 0)
             {
-                using (FileStream fileStream = File.OpenRead(basePath + filename))
+                using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}", basePath, filename)))
                 {
                     Seed(new CreateLoadableFile
                     {
@@ -194,7 +194,7 @@ namespace UCosmic.SeedData
             extension = extension != null ? extension.Substring(1) : null;
             if (_entities.Get<LoadableFile>().Count(x => x.Name == filename) == 0)
             {
-                using (FileStream fileStream = File.OpenRead(basePath + filename))
+                using (FileStream fileStream = File.OpenRead(string.Format("{0}{1}", basePath, filename)))
                 {
                     Seed(new CreateLoadableFile
                     {
