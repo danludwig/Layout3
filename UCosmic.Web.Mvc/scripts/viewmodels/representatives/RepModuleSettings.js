76e6ac5c7df24d791badd4f35192b93cd5b272cc
saibalghosh <saibal.kr.ghosh@gmail.com>
2013-05-07 15:37:03 -0400

    WIP Create RepModuleSettings. Need to implement Email validation.

--- UCosmic.Web.Mvc/scripts/viewmodels/representatives/RepModuleSettings.js
+++ UCosmic.Web.Mvc/scripts/viewmodels/representatives/RepModuleSettings.js
@@ -2,20 +2,13 @@ var ViewModels;
 (function (ViewModels) {
     (function (RepModuleSettings) {
         var RepModuleSettings = (function () {
-            function RepModuleSettings(welcomeMessage, emailMessage) {
-                this.welcomeMessage = welcomeMessage;
-                this.emailMessage = emailMessage;
+            function RepModuleSettings() {
+                this.welcomeMessage = ko.observable();
+                this.emailMessage = ko.observable();
             }
             return RepModuleSettings;
         })();
         RepModuleSettings.RepModuleSettings = RepModuleSettings;        
-        function RepModuleSettings() {
-            var self = this;
-            self.isBound = ko.observable();
-            self.back = function () {
-                history.back();
-            };
-        }
     })(ViewModels.RepModuleSettings || (ViewModels.RepModuleSettings = {}));
     var RepModuleSettings = ViewModels.RepModuleSettings;
 })(ViewModels || (ViewModels = {}));
